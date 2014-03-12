b9792203b89cc017319b73d8b6a758d38b50efc8
MagaBenG <ben--g2@hotmail.com>
2014-03-12 17:08:59 -0400

    Small Changes

---
+++ Program1.h
@@ -0,0 +1,13 @@
+//
+//  Program1.h
+//  
+//
+//  Created by Benjamin DosSantos on 3/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Program1 : NSObject
+
+@end
---
+++ Program1.m
@@ -0,0 +1,10 @@
+// while loop
+int main () {
+    int x = 0;
+    while (x < 10){
+        x++;
+        printf("Count is: %i\n", x);
+    }
+    
+    return 0;
+}
