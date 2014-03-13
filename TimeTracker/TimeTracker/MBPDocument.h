2851048a959483c25427cd6da0178f69ef39260d
MagaBenG <ben--g2@hotmail.com>
2014-03-13 16:11:29 -0400

    Simple †ime Tracker

---
+++ TimeTracker/TimeTracker/MBPDocument.h
@@ -0,0 +1,15 @@
+//
+//  MBPDocument.h
+//  TimeTracker
+//
+//  Created by Benjamin DosSantos on 3/12/14.
+//  Copyright (c) 2014 MagaBenG Productions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MBPDocument : NSPersistentDocument;
+
+@end
+
+
---
+++ TimeTracker/TimeTracker/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TimeTracker
+//
+//  Created by Benjamin DosSantos on 3/12/14.
+//  Copyright (c) 2014 MagaBenG Productions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
